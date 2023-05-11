cf90c91c896b360542c15f07bacc05b5c01a350c
Todd Blythe <todd.blythe@mt.gov>
2023-05-11 14:32:26 -0600

    Add module directories.
    Added directories for MIHM modules (Models, Calibrate, etc.), also started constructing layout of model classes.

---
+++ Models/models.py
@@ -0,0 +1,13 @@
+# Model control classes
+
+class hydromodel():
+    def __init__(selfs):
+
+class prms(hydromodel):
+    def __init__(self):
+
+class rivsysmodel():
+    def __init__(self):
+
+class riverware(rivsysmodel):
+    def __init__(self):
