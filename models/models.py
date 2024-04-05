b39dc39b7f550c4f875731069abb694d2be76ec9
dgketchum <dgketchum@gmail.com>
2024-04-05 10:00:17 -0600

    get basin volumes for water balance components, read in snodas basin swe

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 import os
+import json
 from subprocess import Popen, PIPE, STDOUT
 
 import numpy as np
@@ -7,6 +8,63 @@ import pandas as pd
 from gsflow.prms import PrmsData, PrmsParameters
 from gsflow.control import ControlFile
 from gsflow.output import StatVar
+
+VAR_UNITS = {'ID': None,
+             'runoff': 'cfs',
+             'basin_tmin': 'deg C',
+             'basin_tmax': 'deg C',
+             'basin_ppt': 'inches',
+             'basin_rain': 'inches',
+             'basin_snow': 'inches',
+             'basin_net_ppt': 'inches',
+             'basin_intcp_stor': 'inches',
+             'basin_potet': 'inches',
+             'basin_actet': 'inches',
+             'basin_pweqv': 'inches',  # SWE
+             'basin_snowmelt': 'inches',
+             'basin_snowcov': 'decimal fraction',
+             'basin_sroff': 'inches',
+             'basin_hortonian': 'inches',
+             'basin_infil': 'inches',
+             'basin_soil_moist': 'inches',
+             'basin_recharge': 'inches',
+             'basin_gwstor': 'inches',
+             'basin_gwflow': 'inches',
+             'basin_gwsink': 'inches',
+             'basin_cms': 'cms',
+             'basin_cfs': 'cfs',
+             'basin_ssflow': 'inches',
+             'basin_imperv_stor': 'inches',
+             'basin_lake_stor': 'inches',
+             'basin_ssstor': 'inches',
+             'Date': 'datetime'}
+
+OUTPUT_COLS = ['obs_q',
+               'pred_q',
+               'ppt',
+               'actet',
+               'gwstor',
+               'rain',
+               'snow',
+               'pweqv',
+               'swe_obs',
+               'basin_tmin',
+               'basin_tmax',
+               'soil_moist',
+               'infil',
+               'recharge',
+               'net_ppt',
+               'potet',
+               'snowmelt',
+               'sroff',
+               'hortonian',
+               'intcp_stor',
+               'gwflow',
+               'gwsink',
+               'ssflow',
+               'imperv_stor',
+               'lake_stor',
+               'ssstor']
 
 
 class HydroModel():
@@ -72,25 +130,43 @@ class MontanaPrmsModel(HydroModel):
 
         return success, buff
 
-    def get_statvar(self, snow=None):
+    def get_statvar(self, snow_obs):
 
         self.statvar = StatVar.load_from_control_object(self.control)
         df = self.statvar.stat_df
+        cols = [c.replace('_1', '') for c in df.columns]
+        df.columns = cols
         df.drop(columns=['Hour', 'Minute', 'Second'], inplace=True)
 
-        # cms to m3 per day
-        if self.control.get_record('runoff_units').values[0] == 1:
-            df['runoff_1'] = df['runoff_1'] / 0.028317
+        # cfs to cms per day
+        if self.control.get_record('runoff_units').values[0] == 0:
+            df['runoff'] = df['runoff'] / 0.028317
 
-        df['obs_q_vol_m3'] = 60 * 60 * 24 * df['runoff_1'] / 1e6
-        df['pred_q_vol_m3'] = 60 * 60 * 24 * df['basin_cms_1'] / 1e6
+        df['runoff'][df['runoff'] < 0.0] = np.nan
+
+        # try to get all the water balance components into MCMS per day
+        df['obs_q'] = 60 * 60 * 24 * df['runoff'] / 1e6
+        df['pred_q'] = 60 * 60 * 24 * df['basin_cms'] / 1e6
 
         # ppt in inches, hru_area in acres
         hru_area = self.parameters.get_values('hru_area')[0]
         hru_active = np.count_nonzero(self.parameters.get_values('hru_type'))
-        basin_area = hru_active * hru_area * 43560.
-        ppt_meters = df['basin_ppt_1'] / 39.3701
-        df['ppt_vol_m3'] = basin_area * ppt_meters / 1e6
+        # acres to m2
+        basin_area = hru_active * hru_area.item() * 4046.856
+        vols = []
+
+        def inches_to_million_cubic_meters(col_str_):
+            # inches to meters
+            a = df[col_str_] / 39.3701
+            _name = '_'.join(col_str_.split('_')[1:])
+            # to million cubic meters
+            df[_name] = basin_area * a / 1e6
+            vols.append(_name)
+            return None
+
+        basin_vars = self.statvar.statvar_names
+
+        [inches_to_million_cubic_meters(k) for k, v in VAR_UNITS.items() if k in basin_vars and v == 'inches']
 
         s, e = self.control.get_values('start_time'), self.control.get_values('end_time')
         try:
@@ -100,14 +176,22 @@ class MontanaPrmsModel(HydroModel):
         except ValueError:
             pass
 
-        if snow:
-            s = pd.read_csv(snow, parse_dates=True, index_col='Unnamed: 0', infer_datetime_format=True)
-            s = s.loc[df.index]
-            s /= 25.4
-            s.rename(columns={'mean': 'swe'}, inplace=True)
-            s = s['swe']
-            df = pd.concat([df, s], axis=1, ignore_index=False)
+        with open(snow_obs, 'r') as fp:
+            s = json.load(fp)
 
+        s = [(k, v['0']) for k, v in s.items()]
+        s = sorted(s, key=lambda x: x[0])
+        dt = pd.DatetimeIndex([pd.to_datetime(d[0]) for d in s])
+        s = [a[1] for a in s]
+        s = np.array(s) * basin_area / 1e6
+        s = pd.Series(index=dt, data=s, name='swe_obs')
+        df = pd.concat([df, s], axis=1, ignore_index=False)
+
+        # Agrimet data
+        # 0.04184 mj m2-1 per langley
+
+        df = df.loc['2017-01-01': '2017-12-31', OUTPUT_COLS]
+        # df = df[OUTPUT_COLS]
         self.statvar.stat_df = df
         return self.statvar.stat_df
 
