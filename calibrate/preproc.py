1c5a508c8214e09a6f5aa2c53d7f8600bc32de61
dgketchum <dgketchum@gmail.com>
2024-04-16 15:02:29 -0600

    write swe and q to observation files

--- calibrate/preproc.py
+++ calibrate/preproc.py
@@ -3,6 +3,8 @@ Builds a PRMS model based on an initial set of parameters and writes the observa
 """
 
 import os
+import json
+
 import pandas as pd
 import numpy as np
 
@@ -12,25 +14,42 @@ import warnings
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-project = 'generic_3000'
 
+def preproc(project_root, config, write_model=False, **kwargs):
+    if write_model:
+        build_model(config, return_model=False)
 
-def preproc():
-    project_ = 'generic'
-    wspace = os.path.dirname(os.path.abspath(__file__))
-    conf = os.path.join(wspace, '{}_parameters.toml'.format(project_))
-
-    model = build_model(conf, return_model=True)
-    in_file = os.path.join(model.cfg.data_folder, 'inputs.csv')
+    in_file = os.path.join(project_root, 'input', 'inputs.csv')
     data = pd.read_csv(in_file)
     data.index = list(range(data.shape[0]))
     obs = [c for c in data.columns if 'runoff' in c][0]
     data['q'] = data[obs]
     data = data[['q']]
     print('preproc mean: {}'.format(np.nanmean(data.values)))
-    _file = os.path.join(wspace, 'obs_q.np')
+    _file = os.path.join(project_root, 'obs_q.np')
     np.savetxt(_file, data.values)
     print('Wrote obs to {}'.format(_file))
 
+    # since swe data is incomplete, we'll write it to a DataFrame with a DateTimeIndex
+    if kwargs:
+        for k, v in kwargs.items():
+            with open(v, 'r') as fp:
+                obs = json.load(fp)
+            obs = {kk: vv['0'] for kk, vv in obs.items()}
+            dt = pd.DatetimeIndex([pd.to_datetime(kk, format='%Y%m%d') for kk, vv in obs.items()])
+            obs = [vv for kk, vv in obs.items()]
+            obs = pd.DataFrame(data=obs, index=dt)
+            _file = os.path.join(project_root, 'obs_{}.csv'.format(k))
+            obs.to_csv(_file)
+    pass
 
-preproc()
+
+if __name__ == '__main__':
+    project = 'smith_3000'
+    root = '/home/dgketchum/PycharmProjects/MIHMS/example'
+    proj_dir = os.path.join(root, 'data', project)
+    conf = os.path.join(proj_dir, 'smith_parameters.toml')
+
+    swe_series = '/home/dgketchum/PycharmProjects/MIHMS/example/data/smith_3000/input/snodas.json'
+    observations_ = {'swe': swe_series}
+    preproc(proj_dir, conf, **observations_)
