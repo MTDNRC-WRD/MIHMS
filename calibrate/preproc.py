3815a6704e1e92ef494e8ef313bb3de5a01ade9b
dgketchum <dgketchum@gmail.com>
2024-04-17 14:01:43 -0600

    write swe to np

--- calibrate/preproc.py
+++ calibrate/preproc.py
@@ -8,7 +8,7 @@ import json
 import pandas as pd
 import numpy as np
 
-from example.run import build_model
+from run.run import build_model
 
 import warnings
 
@@ -20,27 +20,32 @@ def preproc(project_root, config, write_model=False, **kwargs):
         build_model(config, return_model=False)
 
     in_file = os.path.join(project_root, 'input', 'inputs.csv')
-    data = pd.read_csv(in_file)
-    data.index = list(range(data.shape[0]))
+    data = pd.read_csv(in_file, index_col='date', parse_dates=True)
     obs = [c for c in data.columns if 'runoff' in c][0]
     data['q'] = data[obs]
-    data = data[['q']]
-    print('preproc mean: {}'.format(np.nanmean(data.values)))
-    _file = os.path.join(project_root, 'obs_q.np')
-    np.savetxt(_file, data.values)
+    q = data[['q']]
+    print('preproc mean: {}'.format(np.nanmean(q.values)))
+    obs_dir = os.path.join(project_root, 'obs')
+    if not os.path.isdir(obs_dir):
+        os.mkdir(obs_dir)
+    _file = os.path.join(obs_dir, 'obs_q.np')
+    np.savetxt(_file, q.values)
     print('Wrote obs to {}'.format(_file))
 
-    # since swe data is incomplete, we'll write it to a DataFrame with a DateTimeIndex
+    # this can be modified to read in more than just swe
     if kwargs:
         for k, v in kwargs.items():
             with open(v, 'r') as fp:
                 obs = json.load(fp)
             obs = {kk: vv['0'] for kk, vv in obs.items()}
-            dt = pd.DatetimeIndex([pd.to_datetime(kk, format='%Y%m%d') for kk, vv in obs.items()])
+            dt = pd.DatetimeIndex([pd.to_datetime(kk, format='%Y%m%d') for kk, vv in obs.items()], tz='UTC')
             obs = [vv for kk, vv in obs.items()]
-            obs = pd.DataFrame(data=obs, index=dt)
-            _file = os.path.join(project_root, 'obs_{}.csv'.format(k))
-            obs.to_csv(_file)
+            obs = pd.Series(data=obs, index=dt, name='swe')
+            match = [i for i in obs.index if i in data.index]
+            data.loc[match, 'swe'] = obs.loc[match]
+            _file = os.path.join(obs_dir, 'obs_{}.np'.format(k))
+            np.savetxt(_file, data['swe'].values)
+            print('Wrote obs to {}'.format(_file))
     pass
 
 
