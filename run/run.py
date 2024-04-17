c68aa94f60c7dc9831919711f11b3f62c7f3f2f5
dgketchum <dgketchum@gmail.com>
2024-04-17 13:59:39 -0600

    refactor run scripts into dir

--- run/run.py
+++ run/run.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import pandas as pd
 
 from models.models import MontanaPrmsModel
@@ -15,10 +16,10 @@ def build_model(config, return_model=False):
         return prms_build
 
 
-def run_model(root, config, project_str, verbose=False):
+def run_model_priors(root, config, project_str, verbose=False):
     prms_build = XyzDistBuild(config)
 
-    data = os.path.join(root, 'data')
+    data = os.path.join(root, '../example/data')
     project = os.path.join(data, '{}_{}'.format(project_str, prms_build.cfg.hru_cellsize))
     prms = MontanaPrmsModel(prms_build.control_file,
                             prms_build.parameter_file,
@@ -32,6 +33,55 @@ def run_model(root, config, project_str, verbose=False):
         stdout_ = None
 
     prms.run_model(stdout_)
+
+
+def run_model_posteriors(root, config, project_str):
+
+    prms_build = XyzDistBuild(config)
+
+    data = os.path.join(root, '../example/data')
+    project = os.path.join(data, '{}_{}'.format(project_str, prms_build.cfg.hru_cellsize))
+    prms = MontanaPrmsModel(prms_build.control_file,
+                            prms_build.parameter_file,
+                            prms_build.data_file)
+
+    # update model params with PEST++ parameter value proposal
+    pest_d = os.path.join(project, 'pest')
+    param_store = os.path.join(pest_d, 'mult')
+    pfiles = [os.path.join(param_store, f) for f in os.listdir(param_store)]
+
+    for f in pfiles:
+        with open(f, 'r') as fp:
+            lines = fp.readlines()
+            strp = lines[1].strip().split(',')
+            k = strp[-3]
+            old_val = prms.parameters.get_values(k)
+            v = float(strp[-1])
+            new_val = v * np.ones_like(old_val)
+            prms.parameters.set_values(k, new_val)
+
+            if old_val is None:
+                continue
+
+            if not isinstance(old_val, float):
+                old_disp = old_val.mean().item()
+            else:
+                old_disp = old_val
+
+            print('{} set: {:.2f} to {:.2f}'.format(k, old_disp, v))
+
+    prms.parameters.write(prms_build.parameter_file)
+    prms.run_model()
+
+    statvar = prms.get_statvar()
+    pred = statvar[['basin_cms']].values
+    np.savetxt(os.path.join(pest_d, 'obs_q.np'), pred)
+
+    idx = statvar['runoff'] >= 0.0
+    valid = statvar.loc[idx]
+    pred, obs = valid['runoff'].mean(), valid['basin_cms'].mean()
+
+    print('Measured Dates, Mean Pred: {:.1f}; Obs {:.1f}'.format(pred, obs))
 
 
 def read_output(config, snow):
@@ -72,7 +122,7 @@ if __name__ == '__main__':
     project_ = 'smith'
     res = 3000
     wspace = os.path.dirname(os.path.abspath(__file__))
-    conf = os.path.join(wspace, 'data', '{}_{}'.format(project_, res), '{}_parameters.toml'.format(project_))
+    conf = os.path.join(wspace, '../example/data', '{}_{}'.format(project_, res), '{}_parameters.toml'.format(project_))
     # build_model(conf)
     # run_model(wspace, conf, project_, verbose=True)
 
