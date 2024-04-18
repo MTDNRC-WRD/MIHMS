d3655a85b208df1536286d203d306da82879c3a5
dgketchum <dgketchum@gmail.com>
2024-04-18 14:07:38 -0600

    add many params

--- prep/prms/default_params.py
+++ prep/prms/default_params.py
@@ -7,6 +7,27 @@ def get_params():
     params = {'basin_sum': {'outlet_sta': (None, None, 0)},
 
               'required': {
+
+                  'adjmix_rain': (0.0, 3.0, 1.0),
+                  'tmax_allsnow': (0.0, 3.0, 0.0),
+                  'ppt_rad_adj': (0.0, 0.5, 0.02),
+                  'srain_intcp': (0.0, 1.0, 0.1),
+                  'wrain_intcp': (0.0, 1.0, 0.1),
+                  'radj_sppt': (0.0, 1.0, 0.44),
+                  'radj_wppt': (0.0, 1.0, 0.5),
+                  'radmax': (0.1, 1.0, 0.8),
+                  'jh_coef_hru': (-99.0, 150.0, 13.0),
+                  'cecn_coef': (0.02, 20.0, 5.0),
+                  'emis_noppt': (0.757, 1.0, 0.757),
+                  'freeh2o_cap': (0.01, 0.2, 0.05),
+                  'potet_sublim': (0.1, 0.75, 0.5),
+                  'carea_max': (0.0, 1.0, 0.6),
+                  'smidx_coef': (0.0, 1.0, 0.005),
+                  'smidx_exp': (0.0, 5.0, 0.3),
+                  'ssr2gw_exp': (0.0, 3.0, 1.0),
+                  'ssr2gw_rate': (0.00001, 999.0, 0.1),
+                  'transp_tmax': (0.0, 1000.0, 1.0),
+
                   'fastcoef_lin': (0.0, 1.0, 0.1),
                   'fastcoef_sq': (0.0, 1.0, 0.8),
                   'pref_flow_den': (0.0, 0.5, 0.0),
@@ -41,12 +62,13 @@ def get_params():
                   'soil_rechr_max_frac': (0.00001, 1.0, 1.0),
                   'soil_moist_init_frac': (0.0, 1.0, 0.0),
                   'soil_rechr_init_frac': (0.0, 1.0, 0.0),
+                  'soil_rechr_max': (0.000001, 20.0, 1.5),
                   'ssstor_init_frac': (0.0, 1.0, 0.0)
               },
 
               'xyz_dist': {'adjust_rain': (-0.5, 3.0, -0.4),
                            'adjust_snow': (-0.5, 3.0, -0.4),
-                           'conv_flag': 0,
+                           'conv_flag': (None, None, 0),
                            'max_lapse': (-100.0, 100.0, 0.0),
                            'min_lapse': (-100.0, 100.0, 0.0),
 
@@ -89,6 +111,46 @@ def get_params():
                            'z_add': (-1.0e7, 1.0e7, 0.0),
                            'z_div': (-1.0e7, 1.0e7, 1.0)}}
     return params
+
+
+def tuning_parameters():
+    tp = {
+        'adjmix_rain': 'swe',
+        'tmax_allrain': 'swe',
+        'tmax_allsnow': 'swe',
+        'dday_intcp': 'swe',
+        'dday_slope': 'swe',
+        'ppt_rad_adj': 'swe',
+        'radj_sppt': 'swe',
+        'radj_wppt': 'swe',
+        'radmax': 'swe',
+        'tmax_index': 'swe',
+        'jh_coef': 'swe',
+        'jh_coef_hru': 'swe',
+        'srain_intcp': 'swe',
+        'wrain_intcp': 'swe',
+        'cecn_coef': 'swe',
+        'emis_noppt': 'swe',
+        'freeh2o_cap': 'swe',
+        'potet_sublim': 'swe',
+        'carea_max': 'swe',
+        'smidx_coef': 'q',
+        'smidx_exp': 'q',
+        'fastcoef_lin': 'q',
+        'fastcoef_sq': 'q',
+        'pref_flow_den': 'q',
+        'sat_threshold': 'q',
+        'slowcoef_lin': 'q',
+        'slowcoef_sq': 'q',
+        'soil2gw_max': 'q',
+        'soil_moist_max': 'q',
+        'soil_rechr_max': 'q',
+        'ssr2gw_exp': 'q',
+        'ssr2gw_rate': 'q',
+        'transp_tmax': 'q',
+        'gwflow_coef': 'q',
+    }
+    return tp
 
 
 def collect_missing_params(stdout):
